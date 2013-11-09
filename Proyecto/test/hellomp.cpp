b082486531b07ee6a8ea81cbc64482e69d0df4d7
frinconr <frinconr95@gmail.com>
2013-11-09 17:51:59 -0600

    first parallel function

--- Proyecto/test/hellomp.cpp
+++ Proyecto/test/hellomp.cpp
@@ -1,14 +1,29 @@
 #include <omp.h>
 #include <iostream>
+
+using namespace std;
  
 int main()
 {	
-	int id;
-	#pragma omp parallel 
+	int n=0;
+	int i,j;
+	//omp_set_schedule(omp_sched_static,3);
+	#pragma omp parallel private(i,j)
 	{
-		id= omp_get_thread_num();
-		cout<<"Greeting from process"<<id<<endl;
+	
+		#pragma omp for ordered schedule (dynamic,4)
+	
+			for(i=0;i<6;++i)
+				for(j=0;j<2;++j)
+				{
+					#pragma omp ordered
+					//{
+					cout<<"n: "<<n<<" num thread"<<omp_get_thread_num()<<endl;
+					//}
+					n++;
+				}
 	}
+	cout<<"n: "<<n<<endl;
 	return 0;
 
 }
